8719c2c223066abaab75033a071fca0a0358e4f7
Kyle <kyle@citrine.io>
2016-03-22 10:15:24 -0700

    Query -> PifQuery

--- src/main/java/io/citrine/jcc/query/PifQuery.java
+++ src/main/java/io/citrine/jcc/query/PifQuery.java
@@ -1,44 +1,14 @@
 package io.citrine.jcc.query;
 
 import com.fasterxml.jackson.annotation.JsonGetter;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonSetter;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 
 /**
- * Class for defining queries against Citrination.
+ * Class for defining queries against PIF records on Citrination.
  *
  * @author Kyle Michel
  */
-public class Query {
-
-
-    public static void main(String[] args) {
-        Query query = new Query().from(0).size(25).system(new SystemQuery()
-                .chemicalFormula(new FieldOperation().extractAs("chemicalFormula")
-                        .filter(new Filter().equal("oxide"))
-                        .filter(new Filter().equal("hydride"))
-                        .filter(new Filter().equal("binary").logic(Logic.MUST))
-                        .filter(new Filter().equal("Li2O2").logic(Logic.NOT).exact(true)))
-                .properties(new PropertiesQuery()
-                        .name(new FieldOperation()
-                                .filter(new Filter().equal("band gap"))
-                                .filter(new Filter().equal("bandgap")))));
-
-
-        try {
-            System.out.println(new ObjectMapper()
-                    .setSerializationInclusion(JsonInclude.Include.NON_NULL)
-                    .configure(SerializationFeature.WRITE_SINGLE_ELEM_ARRAYS_UNWRAPPED, true)
-                    .writerWithDefaultPrettyPrinter().writeValueAsString(query));
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-
+public class PifQuery {
 
     /**
      * Index of the first hit that should be returned.
@@ -47,7 +17,7 @@ public class Query {
      * @return This object.
      */
     @JsonSetter("from")
-    public Query from(final Integer from) {
+    public PifQuery from(final Integer from) {
         this.from = from;
         return this;
     }
@@ -69,7 +39,7 @@ public class Query {
      * @return This object.
      */
     @JsonSetter("size")
-    public Query size(final Integer size) {
+    public PifQuery size(final Integer size) {
         this.size = size;
         return this;
     }
@@ -91,7 +61,7 @@ public class Query {
      * @return This object.
      */
     @JsonSetter("system")
-    public Query system(final SystemQuery system) {
+    public PifQuery system(final SystemQuery system) {
         this.system = system;
         return this;
     }
