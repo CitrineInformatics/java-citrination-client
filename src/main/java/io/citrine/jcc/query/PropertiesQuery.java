38a88d7ae3a660b6dd05b27725363d19183dc1f1
Kyle <kyle@citrine.io>
2016-03-31 11:25:08 -0700

    add conditions to property queries

--- src/main/java/io/citrine/jcc/query/PropertiesQuery.java
+++ src/main/java/io/citrine/jcc/query/PropertiesQuery.java
@@ -1,7 +1,12 @@
 package io.citrine.jcc.query;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonSetter;
+import io.citrine.jcc.util.ListUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Class used to match against a property.
@@ -79,4 +84,49 @@ public class PropertiesQuery extends ValueQuery {
         super.units(filterGroup);
         return this;
     }
+
+    /**
+     * Set the list of conditions operations. This adds to any operations that are already saved.
+     *
+     * @param properties List of {@link ValueQuery} objects.
+     */
+    @JsonSetter("conditions")
+    private void conditions(final List<ValueQuery> properties) {
+        this.conditions = ListUtil.add(properties, this.conditions);
+    }
+
+    /**
+     * Add to the list of conditions operations.
+     *
+     * @param conditions {@link ValueQuery} object to add.
+     * @return This object.
+     */
+    @JsonIgnore
+    public PropertiesQuery conditions(final ValueQuery conditions) {
+        this.conditions = ListUtil.add(conditions, this.conditions);
+        return this;
+    }
+
+    /**
+     * Get an iterable of conditions operations.
+     *
+     * @return Iterable of {@link ValueQuery} objects.
+     */
+    @JsonGetter("conditions")
+    public Iterable<ValueQuery> conditions() {
+        return this.conditions;
+    }
+
+    /**
+     * Get whether an conditions queries exist.
+     *
+     * @return True if any conditions queries exist.
+     */
+    @JsonIgnore
+    public boolean hasConditions() {
+        return ListUtil.hasContent(this.conditions);
+    }
+
+    /** List of conditions queries. */
+    private List<ValueQuery> conditions = new ArrayList<>();
 }
