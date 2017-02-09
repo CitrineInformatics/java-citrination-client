e5458767a8d4a76889510890821d2708d6b6b02e
kjaym <kylemichel@gmail.com>
2017-02-08 18:13:10 -0800

    multi search support

---
+++ src/main/java/io/citrine/jcc/search/core/result/BaseMultiSearchResult.java
@@ -0,0 +1,118 @@
+package io.citrine.jcc.search.core.result;
+
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonSetter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Base class for all multi-search results.
+ *
+ * @param <T> Type of the results.
+ * @author Kyle Michel
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public abstract class BaseMultiSearchResult<T> implements Iterable<T> {
+
+    /**
+     * Set the number of milliseconds that the query took to execute.
+     *
+     * @param took Number of milliseconds for the query to finish.
+     * @return This object.
+     */
+    @JsonSetter("took")
+    public BaseMultiSearchResult setTook(final Long took) {
+        this.took = took;
+        return this;
+    }
+
+    /**
+     * Get the number of milliseconds that a query took to execute.
+     *
+     * @return Long with the number of milliseconds that the query took to finish or a null pointer if that has not
+     *      been set.
+     */
+    @JsonGetter("took")
+    public Long getTook() {
+        return this.took;
+    }
+
+    /**
+     * Set the list of results that were retuned. This overwrites any results that are already saved.
+     *
+     * @param results List of results objects.
+     * @return This object.
+     */
+    @JsonSetter("results")
+    protected BaseMultiSearchResult<T> setResults(final List<T> results) {
+        this.results = results;
+        return this;
+    }
+
+    /**
+     * Add a single result that was returned.
+     *
+     * @param result Result to add to the results set.
+     * @return This object.
+     */
+    @JsonIgnore
+    public BaseMultiSearchResult<T> addResult(final T result) {
+        if (this.results == null) {
+            this.results = new ArrayList<>();
+        }
+        this.results.add(result);
+        return this;
+    }
+
+    /**
+     * Get the number of results that were returned.
+     *
+     * @return Number of results in the returned set.
+     */
+    @JsonIgnore
+    public int getNumResults() {
+        return (this.results == null) ? 0 : this.results.size();
+    }
+
+    /**
+     * Get the list of results that were returned.
+     *
+     * @return List of result objects.
+     */
+    @JsonGetter("results")
+    protected List<T> getResults() {
+        return this.results;
+    }
+
+    /**
+     * Get a result at the set index.
+     *
+     * @param index Index of the result to return.
+     * @return Result at the input index.
+     * @throws IllegalArgumentException if the index is out of bounds.
+     */
+    @JsonIgnore
+    public T getResult(final int index) {
+        if (this.results == null) {
+            throw new IndexOutOfBoundsException("Index out of range: " + index + " of 0");
+        }
+        return this.results.get(index);
+    }
+
+    @Override
+    @JsonIgnore
+    public Iterator<T> iterator() {
+        return (this.results == null) ? Collections.emptyIterator() : this.results.iterator();
+    }
+
+    /** Number of milliseconds that the query took to execute. */
+    private Long took;
+
+    /** List of results that were generated. */
+    private List<T> results;
+}
--- src/main/java/io/citrine/jcc/search/dataset/result/DatasetSearchResult.java
+++ src/main/java/io/citrine/jcc/search/dataset/result/DatasetSearchResult.java
@@ -1,13 +1,9 @@
 package io.citrine.jcc.search.dataset.result;
 
-import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonSetter;
 import io.citrine.jcc.search.core.result.BaseSearchResult;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -25,7 +21,7 @@ import java.util.List;
  *
  * @author Kyle Michel
  */
-public class DatasetSearchResult extends BaseSearchResult implements Iterable<DatasetSearchHit> {
+public class DatasetSearchResult extends BaseSearchResult<DatasetSearchHit>  {
 
     @Override
     @JsonSetter("took")
@@ -41,74 +37,17 @@ public class DatasetSearchResult extends BaseSearchResult implements Iterable<Da
         return this;
     }
 
-    /**
-     * Set the list of hits that were matched. This overwrites any hits that are already saved.
-     *
-     * @param hits List of {@link DatasetSearchHit} objects.
-     * @return This object.
-     */
+    @Override
     @JsonSetter("hits")
     protected DatasetSearchResult setHits(final List<DatasetSearchHit> hits) {
-        this.hits = hits;
+        super.setHits(hits);
         return this;
-    }
-
-    /**
-     * Add a single hit that was matched.
-     *
-     * @param hit {@link DatasetSearchHit} to add to the results set.
-     * @return This object.
-     */
-    @JsonIgnore
-    public DatasetSearchResult addHit(final DatasetSearchHit hit) {
-        if (this.hits == null) {
-            this.hits = new ArrayList<>();
-        }
-        this.hits.add(hit);
-        return this;
-    }
-
-    /**
-     * Get the list of hits that were matched.
-     *
-     * @return List of {@link DatasetSearchHit} objects.
-     */
-    @JsonGetter("hits")
-    protected List<DatasetSearchHit> getHits() {
-        return this.hits;
-    }
-
-    /**
-     * Get the number of hits that were matched.
-     *
-     * @return Number of hits in the result set.
-     */
-    @JsonIgnore
-    public int getNumHits() {
-        return (this.hits == null) ? 0 : this.hits.size();
-    }
-
-    /**
-     * Get a hit at the set index.
-     *
-     * @param index Index of the hit to return.
-     * @return {@link DatasetSearchHit} at the input index.
-     * @throws IllegalArgumentException if the index is out of bounds.
-     */
-    @JsonIgnore
-    public DatasetSearchHit getHit(final int index) {
-        if (this.hits == null) {
-            throw new IndexOutOfBoundsException("Index out of range: " + index + " of 0");
-        }
-        return this.hits.get(index);
     }
 
     @Override
     @JsonIgnore
-    public Iterator<DatasetSearchHit> iterator() {
-        return (this.hits == null) ? Collections.emptyIterator() : hits.iterator();
+    public DatasetSearchResult addHit(final DatasetSearchHit hit) {
+        super.addHit(hit);
+        return this;
     }
-
-    /** List of hits. */
-    private List<DatasetSearchHit> hits;
 }
