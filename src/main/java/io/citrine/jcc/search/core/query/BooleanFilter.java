2e9a77680769f1ce8778bb34ad05089dfe5482f6
kjaym <kylemichel@gmail.com>
2017-07-25 21:15:08 -0700

    add nested boolean filter

--- src/main/java/io/citrine/jcc/search/core/query/BooleanFilter.java
+++ src/main/java/io/citrine/jcc/search/core/query/BooleanFilter.java
@@ -1,6 +1,10 @@
 package io.citrine.jcc.search.core.query;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonSetter;
+import io.citrine.jcc.util.ListUtil;
+
+import java.util.List;
 
 /**
  * Filter that can be applied to boolean-valued fields.
@@ -70,6 +74,81 @@ public class BooleanFilter implements HasLogic {
         return this.equal;
     }
 
+    /**
+     * Set the list of filters. This overrides any values that are already saved.
+     *
+     * @param filter List of {@link BooleanFilter} objects to save.
+     * @return This object.
+     */
+    public BooleanFilter setFilter(final List<BooleanFilter> filter) {
+        this.filter = filter;
+        return this;
+    }
+
+    /**
+     * Add each in a list of filters.
+     *
+     * @param filter List of {@link BooleanFilter} objects to save.
+     * @return This object.
+     */
+    @JsonIgnore
+    public BooleanFilter addFilter(final List<BooleanFilter> filter) {
+        this.filter = ListUtil.add(filter, this.filter);
+        return this;
+    }
+
+    /**
+     * Add to the list of nested filter.
+     *
+     * @param filter {@link Filter} object to add.
+     * @return This object.
+     */
+    @JsonIgnore
+    public BooleanFilter addFilter(final BooleanFilter filter) {
+        this.filter = ListUtil.add(filter, this.filter);
+        return this;
+    }
+
+    /**
+     * Get the number of nested filters have been applied.
+     *
+     * @return Number of filters.
+     */
+    @JsonIgnore
+    public int filterLength() {
+        return ListUtil.length(this.filter);
+    }
+
+    /**
+     * Get an iterable over the list of nested filter.
+     *
+     * @return Iterable of {@link Filter} objects.
+     */
+    @JsonIgnore
+    public Iterable<BooleanFilter> filter() {
+        return ListUtil.iterable(this.filter);
+    }
+
+    /**
+     * Get a single filter.
+     *
+     * @param index Index of the filter to get.
+     * @return Filter at the input index.
+     */
+    @JsonIgnore
+    public BooleanFilter getFilter(final int index) {
+        return ListUtil.get(this.filter, index);
+    }
+
+    /**
+     * Get the list of filters.
+     *
+     * @return List of {@link BooleanFilter} objects or a null pointer if not set.
+     */
+    public List<BooleanFilter> getFilter() {
+        return this.filter;
+    }
+
     /** Logic for applying the filters. */
     private Logic logic;
 
@@ -78,4 +157,7 @@ public class BooleanFilter implements HasLogic {
     
     /** State to match against. */
     private Boolean equal;
+
+    /** List of sub-filters. */
+    private List<BooleanFilter> filter;
 }
